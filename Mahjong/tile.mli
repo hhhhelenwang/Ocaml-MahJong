467a128b42c17f15766eb9a74a560dbde63aeb2f
bz239 <bz239@cornell.edu>
2020-10-17 22:54:12 -0400

    tile module

---
+++ Mahjong/tile.mli
@@ -0,0 +1,27 @@
+(** 
+   Representation of dynamic tile data.
+
+   This module is a representation of each tile, including the kinds, ids, 
+   and discardable status.
+*)
+
+(** The abstract type of values representing tiles. *)
+type t
+
+(** The type of the kind of a tile. *)
+type kind = Man | Pin | Sou | Dragon | Wind
+
+(** The type of the number of a tile. *)
+type number = int
+
+(** The type of the id of a tile. *)
+type id = int
+
+(** The discardable status of a tile *)
+type discarded = bool
+
+(** Raised when an unknown tile is played. *)
+exception UnkownTile of id
+
+(** The discardable status of a tile *)
+val update_status : discarded -> discarded
