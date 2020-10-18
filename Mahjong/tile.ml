9197ec3adfed2e5733a2eae9b1426e8ac114d2b5
bz239 <bz239@cornell.edu>
2020-10-17 23:35:30 -0400

    add record to represent a tile

---
+++ Mahjong/tile.ml
@@ -0,0 +1,15 @@
+(** The type of the id of a tile. *)
+type id = int
+
+(** The type of the kind of a tile. *)
+type kind = Man | Pin | Sou | Dragon | Wind
+
+(** A record to represent tile*)
+type tile = {
+  id : id;
+  kind : kind;
+  number : int;
+  mutable discarded : bool;
+}
+
+type t = tile
