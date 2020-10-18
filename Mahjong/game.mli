f6a26644aca094c970f938d460799de5fef9480e
Jian Wang <jw795@cornel.edu>
2020-10-18 00:00:26 -0400

    game module signature /w struct skeleton

---
+++ Mahjong/game.mli
@@ -0,0 +1,17 @@
+(** 
+   Representation of dynamic game state.
+
+   This module represents the state of a game as it is being played. This 
+   includes the current wall tiles and current state of the player.
+*)
+
+(** The abstract type of values representing the game state. *)
+type t
+
+(** [make_game tiles] is the initial state of the game. It randomly gives
+    each player 13 tiles.*)
+val make_game : Tile.t list -> t
+
+(** [next_state] is the new state of the game after one player has made a 
+    move. *)
+val next_state : t -> t
