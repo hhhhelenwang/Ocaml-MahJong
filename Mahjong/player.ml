3ddc26ada0e32d0884bb4e56d83e6c15f06b4523
Jian Wang <jw795@cornel.edu>
2020-10-22 01:45:28 -0400

    fix weird bug

--- Mahjong/player.ml
+++ Mahjong/player.ml
@@ -5,11 +5,11 @@ type handt={
 }
 
 type t ={
-  id:int;
+  id : int;
   (**State_r tells about state of current player.
      If the player have already riichi,state_r is true
      If the player havn't riichi, state_r state *)
-  mutable state_r: bool;
+  mutable state_r : bool;
 
   (**State_c tells about state of current player.
      If the player have done action chii, this state is true
@@ -36,28 +36,28 @@ let rec check_tile list tid =
   |h :: t -> if (tid = Tile.get_id h) then Some h
     else check_tile t tid
 
-let update_pile tid pile = 
+let update_pile tid pile : Tile.t list = (
   List.filter (fun x -> Tile.get_id x <> tid) pile
+)
 
 let discard_tile player tid =
+  let handt = player.hand_tile.dark in
+  let discardt = player.discard_pile in 
   match (check_tile handt tid) with
   | None -> false
   | Some h -> begin 
-    let handt = player.hand_tile.dark in
-    let discardt = player.discard_pile in 
-    Tile.update_status h;
-    t.hand_tile.dark <- update_pile tid handt;
-    t.discard_pile <- update_pile tid discardt; 
-    true
+      Tile.update_status h;
+      player.hand_tile.dark <- (update_pile tid handt);
+      player.discard_pile <- update_pile tid discardt;
+      true
     end
 
-let rec d_list list=
-   match list with 
-      | [] -> ()
-      | h :: t ->  let h=(Tile.dp h) in d_list t 
+let d_list l = 
+  List.iter Tile.dp l
 
-let display_I t =
-  let hand = t.hand_tile in 
-  let lt = hand.light in d_list lt; 
-(* d_list dt *)
-
+let display_I t = 
+  let hand = t.hand_tile in 
+  let lt = hand.light in 
+  let dt = hand.dark in
+  d_list lt;
+  d_list dt
