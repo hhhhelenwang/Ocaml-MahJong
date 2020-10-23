e67eda34e90edc7acea8af169273c0a4fd66e9c8
Jian Wang <jw795@cornel.edu>
2020-10-23 02:34:47 -0400

    implemented init default deck

--- Mahjong/_build/tile.mli
+++ Mahjong/_build/tile.mli
@@ -1,6 +1,5 @@
 (** 
    Representation of dynamic tile data.
-
    This module is a representation of each tile, including the kinds, ids, 
    and discardable status.
 *)
@@ -11,8 +10,18 @@ type t
 (** The type of the id of a tile. *)
 type id = int
 
+type kind = Man | Pin | Sou | Dragon | Wind
+
 (** Raised when an unknown tile is played. *)
 exception UnknownTile of id
 
 (** The discardable status of a tile *)
 val update_status : t -> unit
+
+(**get id of this tile *)
+val get_id : t -> id
+
+(**display this tile*)
+val dp : t -> unit
+
+val construct : id -> kind -> int -> bool -> t
--- Mahjong/player.mli
+++ Mahjong/player.mli
@@ -1,14 +1,12 @@
 (** The abstract type of values representing player. *)
 type t
 
-
 (** p_id is the identifier of different player.
     Different player should have different id*)
 type id = int
 
-
 (**get the id of this player*)
-val id : t -> id
+val p_id : t -> id
 
 (** riichi state of player*)
 val state_r : t -> bool
@@ -16,14 +14,18 @@ val state_r : t -> bool
 (** chii state of player*)
 val state_c : t -> bool
 
-(** discard a tile with id in t, return exception UnknownTile if id is invalid*)
-val discard_tile: t -> Tile.id -> unit
-
 (** play a tile with id in t, return exception UnknownTile if id is invalid*)
-val play_tile: t -> Tile.id -> unit
+val discard_tile: t -> Tile.id -> bool
 
 (** riichi check if the player can riichi *)
 val check_riichi: t -> bool
 
 (** change status from normal to riichi *)
 val riichi: t -> unit
+
+(** display the tiles of current player*)
+val d_list: Tile.t list -> unit
+
+(** constructor for a player*)
+val init_player: int -> bool -> bool -> Tile.t list -> Tile.t list -> 
+  Tile.t list -> t
