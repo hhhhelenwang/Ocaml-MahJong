c62a8a473771315f8481cb9a27b4fcf4d721dab6
bz239 <bz239@cornell.edu>
2020-11-12 03:00:17 -0500

    seperate to 3

--- Mahjong/player.ml
+++ Mahjong/player.ml
@@ -87,18 +87,20 @@ let init_player id richii chii light dark discard =
     discard_pile = discard;
   }
 
+(* [combine t] returns a list of tiles that is the combination of 
+   light and dark *)
 let combine t = 
   let hand = t.hand_tile in 
   Tile.sort hand.light @ hand.dark
 
+(* [agari t] checks if user can agari or not*)
 let agari t =
   let lst= combine t in
   if (List.length lst <> 14) then false
   else 
     failwith ""
 
-
-
+(* [get_ele lst n] gets the nth tile in user's combined hand tile *)
 let rec get_ele lst n=
   match lst with
   | [] -> []
@@ -107,24 +109,124 @@ let rec get_ele lst n=
       else get_ele lst (n-1)
     end
 
-(* let rec get_element_range lst n l acc = 
-   if l = 0 then acc
-   else get_element_range lst n (l-1) (get_ele lst (n+1)) @ acc
-         failwith "" *)
+(* [get_ele_range lst n l acc] gets a list of tiles starting from n-th to 
+   (n+l)th in user's combined hand tile *)
+let rec get_ele_range lst n l acc = 
+  if l = 0 then acc
+  else get_ele_range lst n (l-1) (get_ele lst (n+1)) @ acc
+
+(* check if user is able to chii. *)
+let chii_legal lst tile = 
+  (* let temp_lst = Tile.sort [tile] :: lst in  *)
+  (* let same_kind = Tile.sort_one_kind tile.kind lst in
+     let same_number = Tile.sort_one_number tile.number same_kind in *)
+  (* filter out all with same kind and number *)
+  (* filter out all with sequencial number and same kind *)
+  failwith ""
+
+(* chii *)
+let chii tile = 
+  failwith ""
+
+
+
+
+(** *)
+(** for ( 12 ) ways to seperate list, check if the choosen 3 form seq or ke, 
+        ( 3  ) *)
+
+(** [sep_list] produces a list of every possible combinations of handtiles
+    lst is list of hand tile
+    n is the size of lst
+    listlist is in the form of [lst; (Man1, Sou 2, Sou 3); (Man2, Man 2, Man2)]::[]
+*)
+(* let get_3_list lst n tup_list=
+   for i = 0 to n do
+    begin for x = i to n do
+        begin for y = x to n do
+            begin if(i<>x && x<>y && i<> y)then 
+                let x1 = get_ele lst i in 
+                let x2 = get_ele lst x in
+                let x3 = get_ele lst y in
+                [x1;x2;x3]::tup_list
+            end
+          done
+        end
+      done
+    end
+   done *)  
+
+(* tile 1 $ [[tile2; til3];[tile 4; tile 5]] gives
+   [[tile1; tile2; til3]; [tile1; tile4; til5]] *)
+let ($) x lst = List.map (fun a -> x::a) lst
+
+(* let rec permutation l r = 
+   match r with 
+   | [] -> [[]]
+   | [x] -> x $ (permutation [] l)
+   | h :: t -> 
+    let set = permutation (h::l) t in
+    (h $ (permutation [] (l@t))) @ set *)
+
+(* n choose 3, n= 12, 12-3, 12-2*3, ... 0(base case) *)
+(* let rec choose lst n acc= 
+   match lst, (List.length lst)discard_tile with
+   | _, size when n == size -> [lst] 
+   | h :: t, _ -> (List.map (fun x -> h :: x) (choose t (n-1) )) @ (choose t n)
+   | [], _ -> []
+               failwith "" *)
+
+type combination= {
+  left_list: Tile.t list;
+  right_list: Tile.t list;
+  c_comb: Tile.t list;
+  pos : Tile.t list list;
+  long_list:  Tile.t list list list;
+}
+
+(**all possibility that divdes 12 tile to 4*3 part *)
+let rec get_3 n1 n2 tup=
+  if n1=0 then
+    let new_t= {tup with pos=[]; long_list= tup.pos :: long_list} in
+    new_tup 
+  else
+  if n2=0 then 
+    let new_t= {tup with c_comb=[]; pos= tup.c_comb::tup.pos; left_list=[]; right_list= tup.left_list} in
+    get_3 (n1-1) 3 new_t
+  else begin
+    match tup.right_list with 
+    |[] -> {tup with c_comb=[]; left_list=[]; right_list=tup.left_list@tup.right_list}
+    |h::t -> let new_comb= tup.c_comb @[h] in 
+      begin if (List.length tup.right_list=n2)
+        then get_3 n1 (n2-1) {tup with c_comb = new_comb; left_list=tup.left_list @[h]; right_list=t }
+        else if 0 = (n2 mod 3)
+        then get_3 n1 (n2-1) {tup with c_comb = new_comb; left_list=tup.left_list @[h]; right_list=t }
+        else
+          let new_tup= (get_3 n1 (n2-1) {tup with c_comb = new_comb; right_list=t }) in
+          get_3 n1 n2 {new_tup with left_list= tup.left_list @[h]; right_list=t }
+      end
+  end
+
+
 
 
 (**case 1: 111 333 555 777 [normal]   3 3 3 3 
    case 2: 223344 567 789          6 3 3
-   case 6: 234 556677 789          3 6 3
+   case 6: 234 556 677 789          3 6 3
    case 7: 234 567 778899          3 3 6
    case 3: 123 333 567 789 [normal]
    case 4: 333344445555        12
    case 5: 222333444 [normal]
+   case 8: 
+   case 9: 56 666 7  x1 x2 x6
+   case 10: 123 455556 789
+   case 11: 123 234 x1x2x4
 *)
 
 let ck_3333 =
   failwith ""
 
-
-let ck_12  x1 x2 x3 x4 x5 x6 x7 x8 x9 x10 x11 x12=
-  (Tile.ck_seq  x1 x5 x9)
+(**check if the list fullfill with 4 part *)
+let ck_12 x1 x2 x3 x4 x5 x6 x7 x8 x9 x10 x11 x12 =
+  (Tile.ck_seq  x1 x5 x9) && (Tile.ck_seq  x2 x6 x10)
+  && (Tile.ck_seq  x3 x7 x11) && (Tile.ck_seq  x4 x8 x12)
--- Mahjong/tile.ml
+++ Mahjong/tile.ml
@@ -105,14 +105,61 @@ let filter_kind kind lst =
 
 (* [sort_one_kind kind lst] returns the sorted list of one kind. 
    [compare t1 t2] compares the number between tiles*)
-let sort_one_kind kind lst = 
+let sorted_one_kind kind lst = 
   let compare t1 t2 = t1.number - t2.number in
   List.sort compare (filter_kind kind lst)
+
+(* [sort_one_number num lst] returns a list of tiles with same 
+   number*)
+let sort_one_number num lst = 
+  List.filter (fun x -> x.number == num) lst
 
 let sort lst =
   let kinds = [Pin; Man; Sou; Wind; Dragon] in 
   let rec helper acc kinds =
     match kinds with
     | [] -> acc
-    | h :: t -> helper (acc @ sort_one_kind h lst) t
+    | h :: t -> helper (acc @ sorted_one_kind h lst) t
   in helper [] kinds
+
+
+(* [get_sequence lst t] returns sequence of all possible 3-tile sequence
+   [lst] is a sorted list with same kind as [t] *)
+(* let rec get_sequence lst num acc=  *)
+(* num -2, num -1, num, num +1, num +2 *)
+(* match lst with 
+   | [] -> acc
+   | h :: t -> begin
+    if (h.number > num - 3 && h.number < num + 3) 
+    then get_sequence t num (h::acc)
+    else get_sequence t num acc
+   end *)
+
+let compare h num = h.number >= num -2 && h.number <= num
+
+(* given a number, find all sequence 
+   (h.number >= num - 2 && h.number <= num)
+   (List.sort_uniq (fun x y -> x.number ) h::acc) *)
+let rec get_seq lst num acc = 
+  match lst with
+  | [] -> begin if List.length acc == 3 then acc else [] end
+  | h :: t -> begin
+      if compare h num
+      then get_seq lst num 
+          (List.sort_uniq (fun x y -> x.number - y.number) (h::acc))
+      else get_seq lst num acc
+    end
+
+(* let all_seq lst num = 
+   let all_f = [ h.number >= num - 2 && h.number <= num;
+              h.number >= num && h.number <= num + 2;
+               h.number >= num - 1 && h.number <= num + 1] in
+   failwith "" *)
+
+
+
+(* let chii_legal lst t = 
+   let same_kind = sorted_one_kind t.kind lst in
+   let same_num = sort_one_number t.number same_kind in
+   let seq_num = get_seq same_kind t.number [] in
+   List.length same_num > 2 || seq_num *)
